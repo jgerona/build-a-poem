4f66ef6824286e673e5f31c956823e6d266db3e5
Joseph <jwillgerona@gmail.com>
2023-04-16 16:50:17 -0700

    seeds edits, created login and profile handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('/api/users/login', {
+    const response = await fetch('/api/profile/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
@@ -30,24 +30,25 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector('#password-signup').value.trim();
 
   if (username && email && password) {
-    const response = await fetch('/api/users', {
+    const response = await fetch('/api/profile', {
       method: 'POST',
       body: JSON.stringify({ username, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
-
+    console.log("here");
     if (response.ok) {
       document.location.replace('/profile');
     } else {
+      
       alert(response.statusText);
     }
   }
 };
 
 document
-  .querySelector('.login-form')
+  .querySelector('#login')
   .addEventListener('submit', loginFormHandler);
 
 document
-  .querySelector('.signup-form')
+  .querySelector('#signup')
   .addEventListener('submit', signupFormHandler);
